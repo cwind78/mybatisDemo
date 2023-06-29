b061a6b9ad883dc557f4f0947848271d8e795827
yony <yony@DESKTOP-17U81VK>
2023-06-29 23:46:16 +0900

    noargsconstructor 추가

--- src/main/java/com/mysql/mybatis/test/demo/user/vo/User.java
+++ src/main/java/com/mysql/mybatis/test/demo/user/vo/User.java
@@ -2,11 +2,13 @@ package com.mysql.mybatis.test.demo.user.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     private String id;
