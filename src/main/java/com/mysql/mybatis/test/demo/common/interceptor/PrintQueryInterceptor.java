a9a012a6e34fe5031684df6f10caa6c1d956a257
kwangsukim <kwangsu.kim@hanatour.com>
2023-06-30 10:39:33 +0900

    git 연동

--- src/main/java/com/mysql/mybatis/test/demo/common/interceptor/PrintQueryInterceptor.java
+++ src/main/java/com/mysql/mybatis/test/demo/common/interceptor/PrintQueryInterceptor.java
@@ -1,15 +1,18 @@
 package com.mysql.mybatis.test.demo.common.interceptor;
 
+import com.mysql.mybatis.test.demo.user.dao.UserDao;
 import com.mysql.mybatis.test.demo.user.vo.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
-import org.apache.ibatis.session.ResultHandler;
-import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+import org.apache.ibatis.session.*;
 
 import java.lang.reflect.Method;
 
@@ -31,10 +34,34 @@ public class PrintQueryInterceptor implements Interceptor {
 
         String sql = mappedStatement.getBoundSql(param).getSql();
         log.info("[MAPPER: {}, METHOD: {}]\n sql: {}, param: {}", mapperId, mthd.getName(), sql, param);
-//        if (mthd.getName().equals("query")) {
-//
-//        }
+
+        if (!mthd.getName().equals("query")) {
+            if (mappedStatement.getCache() == null) {
+                String nameSpace = mappedStatement.getId().substring(0, mappedStatement.getId().lastIndexOf("."));
+                log.info("Namespace Object ID: {}", nameSpace);
+                Cache cache;
+                try {
+                    cache = mappedStatement.getConfiguration().getCache(nameSpace);
+                    if (cache != null) {
+                        MetaObject metaObject = SystemMetaObject.forObject(mappedStatement
+                        metaObject.setValue("cache", cache);
+                    }
+                } catch (IllegalArgumentException e) {
+                    //
+                }
+            }
+        }
 
         return invocation.proceed();
+    }
+
+    SqlSession getSqlSession(Object[] args) {
+        for(Object o: args) {
+            if (o instanceof SqlSession) {
+                return (SqlSession) o;
+            }
+        }
+
+        return null;
     }
 }
--- src/main/java/com/mysql/mybatis/test/demo/user/dao/UserDao.java
+++ src/main/java/com/mysql/mybatis/test/demo/user/dao/UserDao.java
@@ -12,13 +12,13 @@ import java.util.List;
 @Mapper
 @CacheNamespace
 public interface UserDao {
-    @CacheEvict(value = "userCache")//allEntries = true, beforeInvocation=false)
+//    @CacheEvict(value = "userCache")//allEntries = true, beforeInvocation=false)
     public int userRegist(User u);
-    @CacheEvict(value = "userCache")//allEntries = true, beforeInvocation=false)
+//    @CacheEvict(value = "userCache")//allEntries = true, beforeInvocation=false)
     public int userUpdate(User u);
-    @CacheEvict(value = "userCache")//allEntries = true, beforeInvocation=false)
+//    @CacheEvict(value = "userCache")//allEntries = true, beforeInvocation=false)
     public int userDelete(User u);
-    @Cacheable(value = "userCache")
+//    @Cacheable(value = "userCache")
     @Select("select id, name, phone, birth, pwd from user_master where 1 = 1")
     public List<User> userGetList();
 }
