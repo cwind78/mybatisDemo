540ee325840a2ff35bf93a55ad8e957574dd7c6b
yony <yony@DESKTOP-17U81VK>
2023-08-10 21:24:43 +0900

    aspect

--- src/main/java/com/mysql/mybatis/test/demo/common/aspect/MthdAspect.java
+++ src/main/java/com/mysql/mybatis/test/demo/common/aspect/MthdAspect.java
@@ -1,7 +1,18 @@
 package com.mysql.mybatis.test.demo.common.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
 @Component
 public class MthdAspect {
+//	@Around("@annotation(com.mysql.mybatis.test.demo.common.annotation.MthdLogger)")
+//	public Object methodLogAspect(ProceedingJoinPoint joinPoint) throws Throwable {
+//		
+//	}
 }
